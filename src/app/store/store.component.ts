430a06aad8433db572bf372351e20aea1cb1c9cd
Karina Nabor <kari.naborsita1999@gmail.com>
2020-01-03 15:49:15 -0600

    creacion de condiciones

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./store.component.less']
 })
 export class StoreComponent implements OnInit {
-  peliculas = [
+  productos = [
     {
         "producto": "playera",
         "descripcion": "El término ropa se refiere a las prendas de diferentes texturas fabricadas con telas o pieles de animales, usadas por el ser humano para cubrir su cuerpo, incluyendo la ropa impermeable y otra para protegerse del clima adverso. Las prendas pueden ser visibles o no, como en el caso de la ropa interior. En su sentido más amplio, la ropa incluye también algunos otros accesorios como los pañuelos, bolsas o mochilas.",
@@ -48,7 +48,7 @@ export class StoreComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    console.log(this.peliculas);
+    console.log(this.productos);
   }
 
 }
